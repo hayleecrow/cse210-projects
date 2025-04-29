d0530b42bd498e2fc7858602963e58f7027ea8ed
hayleecrow <cro21045@byui.edu>
2025-04-29 12:02:17 -0700

    added grade percentage requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string prec = Console.ReadLine();
+        int x = int.Parse(prec);
+
+        if (x >= 90)
+        {
+            string grade = "A";
+            Console.WriteLine($"{grade}");
+        }
+        else if (x >= 80)
+        {
+            string grade = "B";
+            Console.WriteLine($"{grade}");
+        }
+        else if (x >= 70)
+        {
+            string grade = "C";
+            Console.WriteLine($"{grade}");
+        }
+        else if (x >= 60)
+        {
+            string grade = "D";
+            Console.WriteLine($"{grade}");
+        }
+        else
+        {
+            string grade = "F";
+            Console.WriteLine($"{grade}");
+        }
     }
 }
