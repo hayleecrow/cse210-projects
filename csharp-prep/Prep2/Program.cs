a3c351f53f25205f41a1c7e61e14a46a03a1a94e
hayleecrow <cro21045@byui.edu>
2025-04-29 14:31:18 -0700

    simplified code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,35 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your grade percentage? ");
+        Console.Write("What is your grade percentage? ");
         string prec = Console.ReadLine();
         int x = int.Parse(prec);
+        string letter;
 
         if (x >= 90)
         {
-            string grade = "A";
-            Console.WriteLine($"Your grade is: {grade}");
+            letter = "A";
         }
         else if (x >= 80)
         {
-            string grade = "B";
-            Console.WriteLine($"Your grade is: {grade}");
+            letter = "B";
         }
         else if (x >= 70)
         {
-            string grade = "C";
-            Console.WriteLine($"Your grade is: {grade}");
+            letter = "C";
         }
         else if (x >= 60)
         {
-            string grade = "D";
-            Console.WriteLine($"Your grade is: {grade}");
+            letter = "D";
         }
         else
         {
-            string grade = "F";
-            Console.WriteLine($"Your grade is: {grade}");
+            letter = "F";
         }
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your grade is: {letter}");
 
         if (x >= 70)
         {
