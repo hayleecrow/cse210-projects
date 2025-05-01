dcc35a4f41bfcfba662e477d9ce06d2037265852
hayleecrow <cro21045@byui.edu>
2025-05-01 14:14:36 -0700

    did steps 1 and 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int m = int.Parse(magic);
+
+        int g = 0;
+
+        while (g != m)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            g = int.Parse(guess);
+
+            if (g > m)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (g < m)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
