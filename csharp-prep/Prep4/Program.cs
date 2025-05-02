5734c328d5686d8eaa75771449d0ec24e6822994
hayleecrow <cro21045@byui.edu>
2025-05-01 20:18:48 -0700

    created a number list that the user inputs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
+// using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        // something wrong with this, I think I need to interate it to see the whole list
+        Console.WriteLine(numbers);
     }
 }
