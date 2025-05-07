6979e475c205c527db0bdfb03db0e0f23b0836ee
hayleecrow <cro21045@byui.edu>
2025-05-06 21:14:30 -0700

    called display method

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,7 +10,7 @@ class Program
         job1._startYear = 2020;
         job1._endYear = 2025;
 
-        Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._startYear}-{job1._endYear}");
+        job1.Display();
 
         Job job2 = new Job();
         job2._jobTitle = "Programmer";
@@ -18,6 +18,6 @@ class Program
         job2._startYear = 2017;
         job2._endYear = 2024;
 
-        Console.WriteLine($"{job2._jobTitle} ({job2._company}) {job2._startYear}-{job2._endYear}");
+        job2.Display();
     }
 }
