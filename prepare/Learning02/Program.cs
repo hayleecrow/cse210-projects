e4b7e3c341c38fcc045f348e142187907c781023
hayleecrow <cro21045@byui.edu>
2025-05-06 21:06:37 -0700

    created job 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -11,5 +11,13 @@ class Program
         job1._endYear = 2025;
 
         Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._startYear}-{job1._endYear}");
+
+        Job job2 = new Job();
+        job2._jobTitle = "Programmer";
+        job2._company = "Apple";
+        job2._startYear = 2017;
+        job2._endYear = 2024;
+
+        Console.WriteLine($"{job2._jobTitle} ({job2._company}) {job2._startYear}-{job2._endYear}");
     }
 }
