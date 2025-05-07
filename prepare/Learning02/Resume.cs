67c3cac03605b3c4abd436104a0cde10557b7d9a
hayleecrow <cro21045@byui.edu>
2025-05-06 21:45:10 -0700

    added method and for loop

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -3,4 +3,13 @@ public class Resume
     public string _name;
 
     public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine(_name);
+        for (int i = 0; ;i++)
+        {
+            Console.WriteLine(_jobs[i]._jobTitle);
+        }
+    }
 }
