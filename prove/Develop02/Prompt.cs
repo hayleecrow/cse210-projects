671791b746d9750af7955ea8182b19bf0e4999d9
hayleecrow <cro21045@byui.edu>
2025-05-14 21:25:40 -0700

    added prompts in comment

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -2,8 +2,13 @@ using System;
 
 class Prompt
 {
-    
     public List<Prompt> _prompts = new List<Prompt>();
+
+    /// Who was the most interesting person I interacted with today?
+    /// What was the best part of my day?
+    /// How did I see the hand of the Lord in my life today?
+    /// What was the strongest emotion I felt today?
+    /// If I had one thing I could do over today, what would it be?
 
     public void DisplayRandomPrompt()
     {
