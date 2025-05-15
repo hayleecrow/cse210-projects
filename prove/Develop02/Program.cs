126cdd90b6a18786ab1d0aa95ad38463e0813381
hayleecrow <cro21045@byui.edu>
2025-05-14 20:10:08 -0700

    prompt user to choose an option

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,6 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Journal Program!");
-        
+        int i = 0;
+
+        while (i != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:",
+            "x");
+            int choice = int.Parse(Console.ReadLine());
+            i = choice;
+        }
     }
 }
