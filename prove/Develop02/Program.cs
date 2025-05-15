dd17aed06ef6b4bef490070460282429cf38c345
hayleecrow <cro21045@byui.edu>
2025-05-14 20:33:35 -0700

    added specific actions as choices

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,8 +9,13 @@ class Program
 
         while (i != 5)
         {
-            Console.WriteLine("Please select one of the following choices:",
-            "x");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do? ");
             int choice = int.Parse(Console.ReadLine());
             i = choice;
         }
