03ece0eb62885bd9711f3f2e724eadae240c0566
hayleecrow <cro21045@byui.edu>
2025-05-14 22:23:03 -0700

    added code and comments to 2 methods

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -4,12 +4,15 @@ class Journal
 {
     public List<Entry> _entries = new List<Entry>();
     public string _fileName;
-    public void AddEntry()
+    public void AddEntry(Entry entry)
     {
-
+        _entries.Add(entry);
+        /// refer to this in Entry class at some point
     }
-    public void DisplayAllEntries()
-    {}
+    public void DisplayAllEntries(string date, string entry) /// need to change parameter so it displays the whole list
+    {
+        Console.WriteLine($"Date: {date} - Prompt: {entry}");
+    }
     public void SaveToFile()
     {}
     public void LoadFromFile()
