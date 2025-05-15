852007cba4997d9b85d44c151b292209e8c5f6fb
hayleecrow <cro21045@byui.edu>
2025-05-14 22:06:31 -0700

    created the methods for this class

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -4,4 +4,14 @@ class Journal
 {
     public List<Entry> _entries = new List<Entry>();
     public string _fileName;
+    public void AddEntry()
+    {
+
+    }
+    public void DisplayAllEntries()
+    {}
+    public void SaveToFile()
+    {}
+    public void LoadFromFile()
+    {}
 }
