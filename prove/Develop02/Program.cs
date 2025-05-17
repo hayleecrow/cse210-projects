cf7d99afaf653f0be5a8938c4f4e8b227e66b868
hayleecrow <cro21045@byui.edu>
2025-05-17 09:33:47 -0700

    changed if indicators to strings

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Journal Program!");
-        int i = 0;
+        string i = "0";
         Journal journal = new Journal();
 
-        while (i != 5)
+        while (i != "5")
         {
             Console.WriteLine("Please select one of the following choices:");
             Console.WriteLine("1. Write");
@@ -17,28 +17,27 @@ class Program
             Console.WriteLine("4. Load");
             Console.WriteLine("5. Quit");
             Console.Write("What would you like to do? ");
-            int choice = int.Parse(Console.ReadLine());
-            i = choice;
+            i = Console.ReadLine();
 
-            if (i == 1) /// Write a new entry
+            if (i == "1") /// Write a new entry
             {
                 journal.AddEntry();
             }
-            else if (i == 2) /// Display all entries
+            else if (i == "2") /// Display all entries
             {
                 journal.DisplayAllEntries();
             }
-            else if (i == 3) /// Save a new file
+            else if (i == "3") /// Save a new file
             {
                 journal.SaveToFile();
             }
-            else if (i == 4) /// Load from a file
+            else if (i == "4") /// Load from a file
             {
                 journal.LoadFromFile();
             }
-            else if (i != 5)
+            else if (i != "5")
             {
-                Console.WriteLine($"Sorry, that's not a valid option. Please try again.");
+                Console.WriteLine($"\nSorry, that's not a valid option. Please try again.");
             }
 
             Console.WriteLine();
