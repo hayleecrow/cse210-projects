c68da38cc0f17921d0d761dec6deb86966a6c37f
hayleecrow <cro21045@byui.edu>
2025-05-21 11:40:00 -0700

    created 3 constructors

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,7 +1,22 @@
 using System;
 
-class Fraction()
+public class Fraction
 {
     private int _top;
     private int _bottom;
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _top = bottom;
+    }
 }
