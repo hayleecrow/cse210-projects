8a9b9197b3c708608d7ba483503644aaeb874b6d
hayleecrow <cro21045@byui.edu>
2025-05-21 12:08:22 -0700

    added get and set methods

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -22,4 +22,20 @@ public class Fraction
         _bottom = bottom;
         // Console.WriteLine($"{_top}/{_bottom}");
     }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,7 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetTop());
+        // Console.WriteLine(fraction1.SetTop(4));
+        Console.WriteLine(fraction1.GetBottom());
+        Console.WriteLine(fraction1.GetTop());
+
         Fraction fraction2 = new Fraction(6);
+
+
         Fraction fraction3 = new Fraction(6, 7);
+
     }
 }
