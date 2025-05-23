f2b96fad3ff8a713a7100d227a9a2b1ba9aa2ec3
hayleecrow <cro21045@byui.edu>
2025-05-22 22:04:27 -0700

    tested fraction and decimal methods

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,15 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Fraction fraction1 = new Fraction();
-        Console.WriteLine(fraction1.GetTop());
-        fraction1.SetTop(4);
-        Console.WriteLine(fraction1.GetBottom());
-        fraction1.SetBottom(8);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
 
-        Fraction fraction2 = new Fraction(6);
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
 
-
-        Fraction fraction3 = new Fraction(6, 7);
-
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
