aba651cb7df642561150b9bfe5724bb4bf310052
hayleecrow <cro21045@byui.edu>
2025-05-27 16:53:48 -0700

    created scripture class

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,9 @@
+using System;
+
+// Scripture class Display text has while loop ???
+// initial scripture constructor iterates through text and creates word object for
+/// each word and then adds it to the word list
+class Scripture
+{
+    //
+}
