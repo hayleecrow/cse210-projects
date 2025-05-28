47aefce1950e6d51029a678decc3d703025fb300
hayleecrow <cro21045@byui.edu>
2025-05-27 17:37:26 -0700

    get reference and text and display directly

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5);
+        string scriptureText = "text";
+        string scriptureRef = reference.GetReference();
+        Console.WriteLine($"\n{scriptureRef}\n{scriptureText}");
+        // pass ref and text into scripture when initialize
+        // call DisplayText function
+
+        Console.WriteLine();
     }
 }
