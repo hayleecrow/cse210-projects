b348fecdae60400855c20d905e068c127bcface1
hayleecrow <cro21045@byui.edu>
2025-05-27 17:04:31 -0700

    created planned vars and methods

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Word
+{
+    private string _word;
+    public Word(string word)
+    {
+        //
+    }
+    private string Hide()
+    {
+        return "";
+    }
+    private string Show()
+    {
+        //
+        return "";
+    }
+    private string IsHidden()
+    {
+        //
+        return "";
+    }
+    public void Display()
+    { 
+        //
+    }
+}
