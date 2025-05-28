7d5603b01cc21b75ecfc80e975bb2ee011ad058b
hayleecrow <cro21045@byui.edu>
2025-05-27 17:08:03 -0700

    created planned vars and methods

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -2,6 +2,21 @@ using System;
 
 // Display Reference has if statement for one v or multiple v
 class Reference
-{ 
-    //
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+    public Reference(string book, int chapter, int verse)
+    {
+        //
+    }
+    public Reference(string book, int chapter, int verse, int endVerse)
+    {
+        //
+    }
+    public string GetReference()
+    {
+        return "";
+    }
 }
