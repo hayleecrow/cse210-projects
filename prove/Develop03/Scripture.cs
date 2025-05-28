7288c9c666c7c5f263c4d64cf28f5264d15bc39c
hayleecrow <cro21045@byui.edu>
2025-05-27 17:18:21 -0700

    created planned vars and methods

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,9 +1,29 @@
 using System;
 
-// Scripture class Display text has while loop ???
-// initial scripture constructor iterates through text and creates word object for
-/// each word and then adds it to the word list
+
 class Scripture
 {
-    //
+    private List<Word> _scriptureText = new List<Word>();
+    private string _reference;
+    public Scripture(string text, string reference)
+    {
+        /// Initial scripture constructor iterates through text and creates word object for
+        /// each word and then adds it to the word list
+    }
+    public void DisplayText()
+    {
+        // Scripture class Display text has while loop ???
+    }
+    private void HideWords()
+    {
+        //
+    }
+    private void IsCompletelyHidden()
+    {
+        //
+    }
+    private void ClearConsole()
+    { 
+        //
+    }
 }
