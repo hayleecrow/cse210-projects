723bb63982cb46300c73aa82d7958a5eff8179c6
hayleecrow <cro21045@byui.edu>
2025-05-27 17:52:34 -0700

    can construct and get multi verse ref

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -6,7 +6,7 @@ class Reference
     private string _book;
     private int _chapter;
     private int _verse;
-    private int _endVerse;
+    private int _endVerse = 0;
     public Reference(string book, int chapter, int verse)
     {
         _book = book;
@@ -16,11 +16,23 @@ class Reference
     }
     public Reference(string book, int chapter, int verse, int endVerse)
     {
-        //
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+        Console.WriteLine($"Start: {book} {chapter}:{verse}-{endVerse}");
     }
     public string GetReference()
     {
-        Console.WriteLine($"Gotten: {_book} {_chapter}:{_verse}");
-        return $"{_book} {_chapter}:{_verse}";
+        if (_endVerse == 0)
+        {
+            Console.WriteLine($"Gotten: {_book} {_chapter}:{_verse}");
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        { 
+            Console.WriteLine($"Gotten: {_book} {_chapter}:{_verse}-{_endVerse}");
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
     }
 }
