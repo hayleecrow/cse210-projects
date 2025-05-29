b6d71a82c547d4589ea759dfb593abd6e6192d75
hayleecrow <cro21045@byui.edu>
2025-05-29 16:51:20 -0700

    tested clear console function

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -9,10 +9,15 @@ class Program
     {
         Reference reference = new Reference("D&C", 6, 36, 37);
         string scriptureText = @"Look unto me in every thought; doubt not, fear not. Behold the wounds which pierced my side, and also the prints of the nails in my hands and feet; be faithful, keep my commandments, and ye shall inherit the kingdom of heaven. Amen.";
-        string scriptureRef = reference.GetReference();
-        // Console.WriteLine($"\n{scriptureRef}\n{scriptureText}");
+        string scriptureRef = reference.GetReference(); // puts reference into a traditional format
+        Console.WriteLine($"\n{scriptureRef}\n{scriptureText}");
         Scripture scripture = new Scripture(scriptureText, scriptureRef); // pass ref and text into scripture when initialize
-        // call DisplayText function
+        /// call DisplayText function
+
+        // create while loop
+
+        Console.WriteLine("clearing console...");
+        Console.Clear();
 
         // Console.WriteLine($"Press enter to take out more words or type 'quit' to finish");
         Console.WriteLine();
