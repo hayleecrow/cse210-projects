75100ac6b1002b2e8c088c14eee31c8085b92a28
hayleecrow <cro21045@byui.edu>
2025-06-03 12:11:23 -0700

    called writing class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,6 +7,11 @@ class Program
         Math math = new Math("Samuel Bennett", "Multiplication", "7.3", "8-19");
         string summary = math.GetSummary();
         string list = math.GetHomeworkList();
-        Console.WriteLine($"{summary}\n{list}");
+        Console.WriteLine($"\n{summary}\n{list}");
+
+        Writing writing = new Writing("Mary Waters", "European History", "The Causes of World War II");
+        string summary1 = writing.GetSummary();
+        string info = writing.GetWritingInfo();
+        Console.WriteLine($"\n{summary1}\n{info}\n");
     }
 }
