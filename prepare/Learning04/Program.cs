1bf3caef1005bd58dedc307a29a5786836ead27f
hayleecrow <cro21045@byui.edu>
2025-06-03 11:39:57 -0700

    called math class and assignment methods

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
-        string summary = assignment.GetSummary();
-        Console.WriteLine(summary);
+        Math math = new Math("Samuel Bennett", "Multiplication", "7.3", "8-19");
+        string summary = math.GetSummary();
+        string list = math.GetHomeworkList();
+        Console.WriteLine($"{summary}\n{list}");
     }
 }
