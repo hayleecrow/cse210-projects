33043c6f4b2eb1d0b122ac694b1b89ae59d74862
hayleecrow <cro21045@byui.edu>
2025-06-03 11:10:04 -0700

    created writing assignment class

---
+++ prepare/Learning04/Writing.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Writing
+{
+    private string _title;
+    public Writing(string name, string topic, string title)
+    {
+        //
+    }
+    public string GetWritingInfo()
+    {
+        string info = "";
+        return info;
+    }
+}
