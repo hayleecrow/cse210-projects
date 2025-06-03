175cea164ee2206196a78df2b146c394bcd9a884
hayleecrow <cro21045@byui.edu>
2025-06-03 11:31:03 -0700

    set parent class and defined constructor

--- prepare/Learning04/Math.cs
+++ prepare/Learning04/Math.cs
@@ -1,10 +1,10 @@
 using System;
 
-class Math
+class Math : Assignment
 {
     private string _section;
     private string _problems;
-    public Math(string section, string problems)
+    public Math(string name, string topic, string section, string problems) : base(name, topic)
     {
         _section = section;
         _problems = problems;
