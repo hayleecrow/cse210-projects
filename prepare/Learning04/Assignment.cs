f0e1c5f499e4d0067788465e55efea5cef4c042d
hayleecrow <cro21045@byui.edu>
2025-06-03 11:07:26 -0700

    created assignment class

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Assignment
+{
+    private string _studentName;
+    private string _topic;
+    public Assignment(string name, string topic)
+    { 
+        //
+    }
+    public string GetSummary()
+    {
+        string summary = "";
+        return summary;
+    }
+}
