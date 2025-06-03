f1ba99c7d08471cce15c3c7267481e4373ed50fc
hayleecrow <cro21045@byui.edu>
2025-06-03 11:11:57 -0700

    created math assignment class

---
+++ prepare/Learning04/Math.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Math
+{
+    private string _section;
+    private string _problems;
+    public Math(string section, string problems)
+    {
+        //
+    }
+    public string GetHomewordList()
+    {
+        string list = "";
+        return list;
+    }
+}
