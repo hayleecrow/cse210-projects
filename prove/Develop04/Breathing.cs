b155984a5047d56f4367f11f62ff14c20528b3e4
hayleecrow <cro21045@byui.edu>
2025-06-12 06:53:02 -0700

    rough outline

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -2,20 +2,30 @@ using System;
 
 public class Breathing : Activity
 {
-    public Breathing() : base()
+    public Breathing(string name, string start, string end, int time) : base(name, start, end, time)
     {
-        //
+        // string name = "";
+        // string start = "";
+        // string end = "";
+        // int time = 0;
     }
     public void Run()
     {
-        //
+        base.DisplayStartPrompt();
+
+        // loop for countdown??
+        // repeats breathe in and out for certain amount of time
+
+        base.DisplayEndPrompt();
     }
     private void BreathIn()
     {
-        //
+        Console.Write("Breathe in...");
+        // base.Countdown(4);
     }
     private void BreathOut()
-    { 
-        //
+    {
+        Console.Write("Now breathe out...");
+        // base.Countdown(6);
     }
 }
