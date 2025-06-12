8c381efed0fde0c30bc75774e236a8b0a8382a50
hayleecrow <cro21045@byui.edu>
2025-06-12 06:51:53 -0700

    added starting prompts and ending prompts

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -6,31 +6,40 @@ public class Activity
     private string _startPrompt;
     private string _endPrompt;
     private int _time;
-    public Activity()
+    public Activity(string name, string start, string end, int time)
     {
-        _name = "";
-        _startPrompt = "";
-        _endPrompt = "";
-        _time = 0;
+        _name = name;
+        _startPrompt = start;
+        _endPrompt = end;
+        _time = time;
     }
     public void DisplayStartPrompt()
     {
-        //
+        Console.Clear();
+        Console.WriteLine($"\nWelcome to the {_name} Activity.\n");
+        Console.WriteLine(_startPrompt);
+        Console.Write($"\nHow long, in seconds, would you like for your session? ");
+        _time = int.Parse(Console.ReadLine());
+
+        Console.Clear();
+
+        Console.WriteLine("Get ready...\n");
+        Console.ReadLine(); // ShowSpinner();
     }
     public void DisplayEndPrompt()
     {
-        //
+        Console.WriteLine("Well done!!");
+        Console.ReadLine(); // ShowSpinner();
+
+        Console.WriteLine($"You have completed {_time} seconds of the {_name} Activity.");
+        Console.ReadLine(); // ShowSpinner();
     }
-    public string ShowSpinner()
+    public string ShowSpinner() // pass in a time
     {
         return "";
     }
-    public string Countdown()
+    public string Countdown() // pass in a time
     {
         return "";
-    }
-    public void SetName(string name)
-    { 
-        //
     }
 }
