9f8df9e9e25f6e04e5182edf2f4ab40d038673da
hayleecrow <cro21045@byui.edu>
2025-06-11 17:28:10 -0700

    created planned attributes and methods

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,6 +1,24 @@
 using System;
 
 public class Listing : Activity
-{ 
-    
+{
+    private List<string> _prompts;
+    private List<string> _answers;
+    private int _listNum;
+    public Listing() : base()
+    {
+        //
+    }
+    public void Run()
+    {
+        //
+    }
+    private string GetRandPrompt()
+    {
+        return "";
+    }
+    private void DisplayPrompt()
+    { 
+        //
+    }
 }
--- prove/Develop04/Reflection.cs
+++ prove/Develop04/Reflection.cs
@@ -1,6 +1,31 @@
 using System;
 
 public class Reflection : Activity
-{ 
-    
+{
+    private List<string> _prompts;
+    private List<string> _secondPrompts;
+    public Reflection()
+    {
+        //
+    }
+    public void Run()
+    {
+        //
+    }
+    private string GetRandPrompt()
+    {
+        return "";
+    }
+    private string GetSecondRandPrompt()
+    {
+        return "";
+    }
+    private void DisplayPrompt()
+    {
+        //
+    }
+    private void DisplaySecondPrompt()
+    { 
+        //
+    }
 }
