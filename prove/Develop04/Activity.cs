bf60e8ef966b9748ec235a6f8be6aff08b65d7b0
hayleecrow <cro21045@byui.edu>
2025-06-11 17:12:53 -0700

    created planned attributes and methods

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,6 +1,36 @@
 using System;
 
 public class Activity
-{ 
-    
+{
+    private string _name;
+    private string _startPrompt;
+    private string _endPrompt;
+    private int _time;
+    public Activity()
+    {
+        _name = "";
+        _startPrompt = "";
+        _endPrompt = "";
+        _time = 0;
+    }
+    public void DisplayStartPrompt()
+    {
+        //
+    }
+    public void DisplayEndPrompt()
+    {
+        //
+    }
+    public string ShowSpinner()
+    {
+        return "";
+    }
+    public string Countdown()
+    {
+        return "";
+    }
+    public void SetName(string name)
+    { 
+        //
+    }
 }
