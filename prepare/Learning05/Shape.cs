349b11622c565dcd3f1fb046aa5c6ee2ddbd92a8
hayleecrow <cro21045@byui.edu>
2025-06-18 19:14:00 -0700

    added getter and setter and constructor

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -2,10 +2,21 @@ using System;
 
 class Shape
 {
-    private string paperColor;
-    private float sideLength;
-    public float ComputeArea() // don't store as member variable
+    private string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public virtual double GetArea() // don't store as member variable
     {
         return 0;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
     }
 }
