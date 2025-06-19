51a8c9156a393a1712509f9d1085edacb9bf4991
hayleecrow <cro21045@byui.edu>
2025-06-18 19:24:16 -0700

    used math library to calc area

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -1,6 +1,15 @@
 using System;
 
-class Circle
-{ 
+class Circle : Shape
+{
     // color and radius
+    private double _radius;
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
+    }
 }
