0758082a47df6859bb8d090040983247e9bb4889
hayleecrow <cro21045@byui.edu>
2025-06-18 19:36:37 -0700

    fin

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+
+        // store shapes in a list
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Pink", 5),
+            new Rectangle("Purple", 4, 10),
+            new Circle("Yellow", 3),
+        };
+
+        // iterate through them and display area
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} -- Area: {shape.GetArea()}");
+        }
     }
 }
