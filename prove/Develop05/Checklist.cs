fa297d54dd8120ed1802902b8eb68d430d417d2e
hayleecrow <cro21045@byui.edu>
2025-06-28 22:50:55 -0700

    updated constructor, save and display strings

--- prove/Develop05/Checklist.cs
+++ prove/Develop05/Checklist.cs
@@ -5,14 +5,14 @@ class Checklist : Goal
     private int _bonusPoints;
     private int _targetCount;
     private int _currentCount;
-    public Checklist(string goalType, string name, string description, int points) : base(goalType, name, description, points)
+    public Checklist(string goalType, string name, string description, int points, int bonus, int target) : base(goalType, name, description, points)
     {
-        _bonusPoints = 0;
-        _targetCount = 0;
+        _bonusPoints = bonus;
+        _targetCount = target;
         _currentCount = 0;
     }
     public Checklist(string goalType, string name, string description, int points, int bonus, int target, int current) : base(goalType, name, description, points)
-    { 
+    {
         _bonusPoints = bonus;
         _targetCount = target;
         _currentCount = current;
@@ -27,6 +27,10 @@ class Checklist : Goal
     }
     public override string SaveString()
     {
-        return $"{base.SaveString()} ";
+        return $"{base.SaveString()}~~{_bonusPoints}~~{_targetCount}~~{_currentCount}";
+    }
+    public override string DisplayString()
+    {
+        return $"{base.DisplayString()} -- Currently Completed: {_currentCount}/{_targetCount}";
     }
 }
