c6bbe0ff3f03525ab91dedc3d5333769c1f778d0
hayleecrow <cro21045@byui.edu>
2025-06-28 22:47:17 -0700

    displays, saves, and loads goals

--- prove/Develop05/Game.cs
+++ prove/Develop05/Game.cs
@@ -90,16 +90,101 @@ Which type of goal would you like to create? ");
     }
     public void DisplayGoals()
     {
-        //
+        int counter = 0;
+
+        foreach (Goal goal in _goals)
+        {
+            counter++;
+
+            Console.WriteLine($"The goals are:");
+            Console.WriteLine($"{counter}. {goal.DisplayString()}");
+        }
     }
     public void SaveGoals()
     {
-        //
+        Console.Write($"What is the name of your file? ");
+        string filename = Console.ReadLine();
+
+        // will create new file if file doesn't already exist
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            outputFile.WriteLine(_totalScore);
+
+            foreach (Goal goal in _goals)
+            {
+                // writes the entries onto the file
+                outputFile.WriteLine(goal.SaveString());
+            }
+        }
+
+        Console.WriteLine($"File saved.");
     }
     public void LoadGoals()
     {
         // 1. for reading the file, indicate line[0] = total points and then remove it from the array, so you can iterate through the rest of the lines
         // 2. also for reading each line, first seperate based on the "colon" then seperate based on the "commas"
+        Console.Write($"What is the name of the file? ");
+        string filename = Console.ReadLine();
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        Console.WriteLine();
+
+        _goals.Clear(); // clears any current goals
+
+        // _totalScore = int.Parse(lines[0]);
+
+        foreach (string line in lines)
+        {
+            string[] first = line.Split("|"); // first split to get goal type
+
+            if (first.Count() == 1)
+            {
+                _totalScore = int.Parse(line);
+            }
+            else if (first.Count() > 1)
+            {
+                if (first[0] == "Simple Goal")
+                {
+                    string[] second = first[1].Split("~~");
+
+                    string name = second[0];
+                    string description = second[1];
+                    int points = int.Parse(second[2]);
+                    bool completion = bool.Parse(second[3]);
+
+                    Simple goal = new Simple("Simple Goal", name, description, points, completion);
+                    _goals.Add(goal);
+                }
+                else if (first[0] == "Eternal Goal")
+                {
+                    string[] second = first[1].Split("~~");
+
+                    string name = second[0];
+                    string description = second[1];
+                    int points = int.Parse(second[2]);
+                    int total = int.Parse(second[3]);
+                    int times = int.Parse(second[4]);
+
+                    Eternal goal = new Eternal("Eternal Goal", name, description, points, total, times);
+                    _goals.Add(goal);
+                }
+                else if (first[0] == "Checklist Goal")
+                {
+                    string[] second = first[1].Split("~~");
+
+                    string name = second[0];
+                    string description = second[1];
+                    int points = int.Parse(second[2]);
+                    int bonus = int.Parse(second[3]);
+                    int target = int.Parse(second[4]);
+                    int current = int.Parse(second[5]);
+
+                    Checklist goal = new Checklist("Checklist Goal", name, description, points, bonus, target, current);
+                    _goals.Add(goal);
+                }
+            }
+        }
+
+        Console.WriteLine($"The file {filename} has been loaded.");
     }
     public void NewEvent()
     { 
